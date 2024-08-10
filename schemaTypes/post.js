c0efa46d6468279e32887e3bffd0b209c6d4aa02
7uran <turan.official21@gmail.com>
2024-08-11 03:20:26 +0400

    feat:Added sanity

---
+++ schemaTypes/post.js
@@ -0,0 +1,42 @@
+import { defineField, defineType } from 'sanity'
+
+export default defineType({
+  name: 'post',
+  title: 'Post',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'title',
+      title: 'Title',
+      type: 'string',
+    }),
+    defineField({
+      name: 'type',
+      title: 'Type',
+      type: 'string',
+    }),
+   
+    defineField({
+      name: 'mainImage',
+      title: 'Main image',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+    }),
+
+
+  ],
+
+  preview: {
+    select: {
+      title: 'title',
+      author: 'author.name',
+      media: 'mainImage',
+    },
+    prepare(selection) {
+      const { author } = selection
+      return { ...selection, subtitle: author && `by ${author}` }
+    },
+  },
+})
